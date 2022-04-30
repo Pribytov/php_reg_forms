94e84199d4e227fd20ea7997d315fa2046b27e0b
Igor Pribytov <igor-pribytov@yandex.ru>
2022-05-01 02:43:44 +0300

    Обновил ответы на сторону фронта для регистрации

--- reg_obr.php
+++ reg_obr.php
@@ -1,14 +1,28 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
-$name = $_POST['name'];
-$lastname = $_POST['lastname'];
-$email = $_POST['email'];
-$pass = $_POST['pass'];
+$mysqli = mysqli_connect("localhost", "expisjiy_0207-1", "12345", "expisjiy_0207-1");
 
-echo "Имя $name<br>
-Фамилия $lastname<br>
-Email $email<br>
-Пароль $pass"
+if ($mysqli == false){
+    print("error");
+} else {
+
+    $name = $_POST['name'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+
+    $result = $mysqli->query("SELECT * FROM `users` WHERE `email` = '$email' AND `pass` = '$pass'");
+    //var_dump($result->num_rows);
+
+    if ($result->num_rows != 0) {
+        print("exist");
+    } else {
+        $mysqli->query("INSERT INTO `users`(`name`, `lastname`, `email`, `pass`)
+            VALUES ('$name', '$lastname', '$email', '$pass')");
+        print("ok");
+    }
+}
+
 
 ?>
