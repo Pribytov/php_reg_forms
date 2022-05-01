4de8af10802fe0b4ab59cd11c7e2faeb9b6fdb08
Igor Pribytov <igor-pribytov@yandex.ru>
2022-05-02 02:30:34 +0300

    Авторизация с AJAX

--- auth_obr.php
+++ auth_obr.php
@@ -1,10 +1,19 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
-$email = $_POST['email'];
-$pass = $_POST['pass'];
+$mysqli = mysqli_connect("localhost", "expisjiy_0207-1", "12345", "expisjiy_0207-1");
 
-Email $email<br>
-Пароль $pass";
+if ($mysqli == false) {
+    print("error");
+} else {
 
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+
+    $result = $mysqli->query("SELECT * FROM `users` WHERE `email` = '$email' AND `pass` = '$pass'");
+
+    if ($result->num_rows == 1) {
+        print("ok");
+    }
+}
 ?>
